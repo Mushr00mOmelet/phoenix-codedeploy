bb09e31110be0570cd1af75bf79472fa60225f3b
Monica Hirst <monica@societyofgrownups.com>
2016-05-14 23:31:17 -0400

    simplify install script

--- deploy/after-install.sh
+++ deploy/after-install.sh
@@ -9,17 +9,13 @@ echo "Customizing environment: $ENVIRONMENT"
 
 # Setup variables depending on what environment
 case $ENVIRONMENT in
-    squirtle)
-        S3=squirtle-s3-buckets-appconfigbucket-hsb31mlv52sx
-        export MIX_ENV=squirtle
+    blue)
+        S3=monica-blue
+        export MIX_ENV=blue
     ;;
-    charmander)
-        S3=charmander-s3-buckets-appconfigbucket-159qk8jolnz49
-        export MIX_ENV=charmander
-    ;;
-    bulbasaur)
-        S3=bulbasaur-s3-buckets-appconfigbucket-19b4kyxchtk7b
-        export MIX_ENV=bulbasaur
+    red)
+        S3=monica-red
+        export MIX_ENV=red
     ;;
     production)
         S3=production-s3-buckets-appconfigbucket-wyvb0uh5uocb
@@ -43,27 +39,14 @@ python ./awslogs-agent-setup.py \
     --configfile /tmp/awslogs.conf
 service awslogs restart
 
-# Monitoring
-# relies on being run in /home/root
-curl http://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.1.zip -O
-unzip -o CloudWatchMonitoringScripts-1.2.1.zip
-install -m 644 "$SOURCE_DIR"/awsmoncron /etc/cron.d/awsmon
-
 # Move into the app directory
 cd /opt/phoenix-codedeploy
 
 # Pull in secrets from S3 Bucket
 aws --region=us-east-1 s3 cp s3://$S3/monica-app-$ENVIRONMENT.secret.exs /opt/phoenix-codedeploy/config/$MIX_ENV.secret.exs
 
-# Copy over the upstart script, downloading and setting variables as needed
-export MANDRILL_KEY=CfXexuUzxPcLKR3CuYaw4A  # fallback testing key
-aws --region=us-east-1 s3 cp s3://$S3/monica-app-$ENVIRONMENT.secrets.sh /opt/phoenix-code-deploy/deploy/secrets.sh || true
-test -f /opt/phoenix-cd-demo/deploy/secrets.sh && source /opt/phoenix-codedeploy/deploy/secrets.sh
-awk -v mix="$MIX_ENV" -v mandrill="$MANDRILL_KEY" '{
-    gsub(/MIX_ENV_VALUE/, mix)
-    gsub(/MANDRILL_KEY_VALUE/, mandrill)
-    print
-}' /opt/phoenix-codedeploy/deploy/monica-app-upstart.conf >/etc/init/monica-app.conf
+# Copy over the upstart script and set MIX_ENV correctly
+sed "s/MIX_ENV_VALUE/$MIX_ENV/"  /opt/phoenix-codedeploy/deploy/monica-app-upstart.conf >/etc/init/monica-app.conf
 
 export HOME=/root
 mix local.hex --force
