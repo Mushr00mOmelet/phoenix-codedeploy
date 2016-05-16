c0c85e7cddcba43aeaf4b220a5602c25bb6232b3
Monica Hirst <dragongirl777@gmail.com>
2016-05-15 23:02:46 -0400

    simplify start

--- deploy/application-start.sh
+++ deploy/application-start.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-# Check if nginx service is running
-if pgrep nginx >/dev/null 2>&1
-  then
-    sudo service nginx stop
-  else
-    echo 'Service is not running yet... moving on'
-fi
-
 sudo start monica-app
